49ba2a4f1807970cf6f0790f31385d93152663e3
sbkim052 <sbkim052@gmail.com>
2020-05-06 15:48:07 +0900

    update class.c

--- class.c
+++ class.c
@@ -25,3 +25,25 @@ int addClass(Classes *c){
 void  readClass(Classes *c){
 	printf("%4s %4s %3d %3s %3d\n",c->name, c->prof, c->class_no, c->day, c->time); 
 }
+
+int updateClass(Classes *c){
+
+	printf("\n");
+	printf("과목명은? ");
+	scanf("%[^\n]s",c->name);
+	getchar();
+	printf("교수님 성함은? ");
+	scanf("%[^\n]s",c->prof);
+	getchar();
+	printf("분반은? ");
+	scanf("%d",&c->class_no);
+	getchar();
+	printf("수업 요일은? ");
+	scanf("%[^\n]s",c->day);
+	getchar();
+	printf("수업 교시는? ");
+	scanf("%d",&c->time);
+	getchar();
+	printf("==> 수정됨\n");
+	return 1;
+}
