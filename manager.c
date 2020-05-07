0c52d213a36f4177a4372480000bc810dedc6a13
Lee shinbi <21600510@handong.edu>
2020-05-07 13:37:43 +0900

    Update loadData

--- manager.c
+++ manager.c
@@ -15,7 +15,7 @@ int selectMenu(){
 
 void listSubject(Classes *c, int count){
 //class는 교시, Day는 요일, Section은 분반
-	printf("\nNo. Subject       Professor  Class  Day  Section\n");	 
+	printf("\nNo.  Subject    Professor   Class  Day  Section\n");	 
 	printf("==================================================\n");
 	for(int i=0 ; i<count ; i++){
 		if(c[i].time == -1 || c[i].class_no == -1) continue;
@@ -38,7 +38,7 @@ void saveData(Classes *c, int count){
 	FILE* fp;
 	fp = fopen("Classes.txt", "wt");
 	for(int i=0 ; i<count ; i++){
-			fprintf(fp, "%2d %5s %2d %5s %7s\n", c[i].class_no, c[i].day, c[i].time, c[i].prof, c[i].name);
+			fprintf(fp, "%3d %8s %3d %8s %10s\n", c[i].class_no, c[i].day, c[i].time, c[i].prof, c[i].name);
 	}
 	fclose(fp);
 	printf("저장됨\n");
@@ -54,7 +54,7 @@ int loadData(Classes *c){
 		return 0;
 	}
 	for(;;count++){
-		fscanf(fp, "%2d %5s %2d %5s %[^\n]s\n",&c[count].class_no, c[count].day, &c[count].time, c[count].prof, c[count].name ); 
+		fscanf(fp, "%3d %8s %3d %8s %[^\n]s\n",&c[count].class_no, c[count].day, &c[count].time, c[count].prof, c[count].name ); 
 		if(feof(fp)) break;
 	}
 	fclose(fp);
