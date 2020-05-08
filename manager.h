2afbb2bf00278d4b4e2cf8f1413d924c646efb4a
Lee shinbi <21600510@handong.edu>
2020-05-08 15:17:39 +0900

    Add

--- manager.h
+++ manager.h
@@ -6,3 +6,7 @@ int selectDataNo(Classes *c, int count);
 
 void saveData(Classes *c, int count);
 int loadData(Classes *c);
+
+void Calculation(Classes *c, int count);
+void ReadDayList(Classes *c);
+void SearchSameTime(Classes *c, int count);
