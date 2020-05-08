7415f74d83ea05414a6eefc1539cc121b8aa942d
Lee shinbi <21600510@handong.edu>
2020-05-08 17:07:25 +0900

    Update manager.h

--- manager.h
+++ manager.h
@@ -1,12 +1,12 @@
 #include "class.h"
 
-int selectMenu();
-void listSubject(Classes *c, int count);
-int selectDataNo(Classes *c, int count);
+int selectMenu();	//메뉴함수
+void listSubject(Classes *c, int count);	//리스트출력함수
+int selectDataNo(Classes *c, int count);	//숫자선택함수
 
-void saveData(Classes *c, int count);
-int loadData(Classes *c);
+void saveData(Classes *c, int count);	//파일저장함수
+int loadData(Classes *c);	//파일로드함수
 
-void Calculation(Classes *c, int count);
-void ReadDayList(Classes *c);
-void SearchSameTime(Classes *c, int count);
+void Calculation(Classes *c, int count);	//학점계산함수
+void ReadDayList(Classes *c);	//요일별과목리스트함수
+void SearchSameTime(Classes *c, int count);	//겹치는시간과목찾는함수
