6aac43c64e7fcb047ebf4edeee0f38fd78d2a34c
16shinbi <21600510@handong.edu>
2020-05-12 14:15:37 +0900

    Edit type

--- manager.h
+++ manager.h
@@ -7,7 +7,7 @@ int selectDataNo(Classes *c, int count);	//숫자선택함수
 void saveData(Classes *c, int count);	//파일저장함수
 int loadData(Classes *c);	//파일로드함수
 
-void Search_control(Classes *c, int count); // 검색을 총괄하는 함수
+int  Search_control(Classes *c, int count); // 검색을 총괄하는 함수
 
 void Calculation(Classes *c, int count);	//학점계산함수
 void ReadDayList(Classes *c, int count);	//요일별과목리스트함수
